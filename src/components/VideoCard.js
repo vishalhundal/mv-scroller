f3e42244a53269a1417801abee33c529fa19de21
vishalhundal <vishalhundal@gmail.com>
2020-06-16 17:18:50 -0400

    Edited navbar for color

---
+++ src/components/VideoCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import Card from "react-bootstrap/Card";
+
+export default class VideoCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    };
+
+    render() {
+        return (
+            <div>
+                <h5>Hottest this month:</h5>
+            </div>
+        );
+    };
+};
